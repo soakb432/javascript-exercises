29cd8f7bf6f6528f23e584e61c56947c15571f66
soakb432 <107282874+SoakB@users.noreply.github.com>
2024-09-17 18:50:21 -0400

    Create rest parameter

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function(array, num) {
+const removeFromArray = function(array, ...num) {
+  let index;
 
+  for (const n of num) { // rest parameter
+    index = array.indexOf(n);
+    array.splice(index, 1);
+  }
+
+  return array;
 };
 
 // Do not edit below this line
