3e82e475d04785e5e53dc8dab1cb6ec9d1930dc5
soakb432 <107282874+SoakB@users.noreply.github.com>
2024-09-17 00:14:25 -0400

    Concatenate from the end of the array

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
 const reverseString = function(string) {
   let text = string.split(""); // array
   let reverseString = "";
+  
+  while (text.length) {
+    reverseString += text.pop();
+  }
+
+  return reverseString;
 };
 
 // Do not edit below this line
