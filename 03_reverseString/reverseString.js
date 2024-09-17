e57ed7ef5b1311eccd312bc8df4202afd6b45eab
soakb432 <107282874+SoakB@users.noreply.github.com>
2024-09-17 00:12:13 -0400

    Create necessary arguments & variables

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let text = string.split(""); // array
+  let reverseString = "";
 };
 
 // Do not edit below this line
