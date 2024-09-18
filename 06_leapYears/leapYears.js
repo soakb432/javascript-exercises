4b377664503b1ef6776dfd3af127cb927e80df8e
soakb432 <107282874+SoakB@users.noreply.github.com>
2024-09-17 21:28:31 -0400

    Create variables for conditions & check them together

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
 const leapYears = function(year) {
+  const isYearDivisibleByFour = year % 4 === 0;
+  const isCentury = year % 100 === 0;
+  const isYearDivisibleByFourHundred = year % 400 === 0;
 
+  return (isYearDivisibleByFour && (!isCentury || isYearDivisibleByFourHundred))
 };
 
 // Do not edit below this line
