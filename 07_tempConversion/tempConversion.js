ef9512184bb57012eb8b30d99c89ac209ae17b01
soakb432 <107282874+SoakB@users.noreply.github.com>
2024-09-17 21:56:40 -0400

    Round temperature to one decimal

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
 const convertToCelsius = function(fahr) {
-  return (fahr - 32) * (5/9);
+  let cels = (fahr - 32) * (5/9);
+  return Math.round(cels * 10) / 10;
 };
 
 const convertToFahrenheit = function(cels) {
-  return cels * (9/5) + 32;
+  let fahr = cels * (9/5) + 32;
+  return Math.round(fahr * 10) / 10;
 };
 
 // Do not edit below this line
