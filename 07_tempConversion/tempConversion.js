5050bf2a5bc4781c506aa4e9b06b5ae3108fc716
soakb432 <107282874+SoakB@users.noreply.github.com>
2024-09-17 21:48:43 -0400

    Convert from celsius to fahrenheit

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -2,7 +2,8 @@ const convertToCelsius = function(fahr) {
   return (fahr - 32) * (5/9);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cels) {
+  return cels * (9/5) + 32;
 };
 
 // Do not edit below this line
