e20e17e59e731e1e4cfacd8bdd5b4eca45ba42b4
soakb432 <107282874+SoakB@users.noreply.github.com>
2024-09-17 20:26:28 -0400

    Check which argument is bigger

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,9 +1,20 @@
-const sumAll = function(start, end) {
+const sumAll = function(a, b) {
   let sum = 0;
+  let start;
+  let end;
+
+  if (a > b) {
+    start = b;
+    end = a;
+  } else {
+    start = a;
+    end = b;
+  }
+
   for (let i = start; i <= end; i++) {
     sum += i;
   }
-  return sum
+  return sum;
 };
 
 // Do not edit below this line
