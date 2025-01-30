3effe50e9db10622221b488355375efc26ae39d1
soakb432 <107282874+soakb432@users.noreply.github.com>
2025-01-30 17:49:38 -0400

    Make letters lower-case

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,10 +1,11 @@
 const palindromes = function (word) {
-    word = word.split("")
+    word = word.toLowerCase()
+        .split("")
         .filter(char => /[^\W_]/.test(char))
         .join();
     
     let [start, end] = [0, word.length - 1];
-    
+
     for (let i = 0; i < word.length; i++) {
         if (word[start] !== word[end]) {
             return false;
