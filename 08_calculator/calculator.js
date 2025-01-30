ac419f44893d44c755f69119e9e9af32b881196f
soakb432 <107282874+soakb432@users.noreply.github.com>
2025-01-30 16:11:03 -0400

    Create sum function

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,8 +6,8 @@ const subtract = function(a, b) {
   return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total, curr) => total + curr, 0);
 };
 
 const multiply = function() {
