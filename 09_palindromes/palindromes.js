cb4b2083934f43a2698151925bf693922cbfdb6f
soakb432 <107282874+soakb432@users.noreply.github.com>
2025-01-30 17:30:58 -0400

    Check for single words

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let [start, end] = [0, string.length - 1];
+    for (let i = 0; i < string.length; i++) {
+        if (string[start] !== string[end]) {
+            return false;
+        } else {
+            start++;
+            end--;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
