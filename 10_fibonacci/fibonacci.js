b5d14e5e748b28f25a28a23dbc39687a0fa5c3b0
soakb432 <107282874+soakb432@users.noreply.github.com>
2025-01-31 14:47:29 -0400

    Test for negative numbers

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,7 @@
 const fibonacci = function(n) {
-    if (n == 0) {
+    if (n < 0) {
+        return "OOPS";
+    } else if (n == 0) {
         return 0;
     } else if (n == 1 || n == 2) {
         return 1;
